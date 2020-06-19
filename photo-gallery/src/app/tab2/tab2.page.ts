980f9cacf78fea6e17f32545ee1a4ac96fb2f274
msagar9 <sagar.maniya@faciletechnolab.com>
2020-06-19 12:48:28 +0530

    delete

--- photo-gallery/src/app/tab2/tab2.page.ts
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,8 +8,30 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  public async showActionSheet(photo, position) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, 
+    public actionSheetController: ActionSheetController) {}
   ngOnInit() {
     this.photoService.loadSaved();
   }
